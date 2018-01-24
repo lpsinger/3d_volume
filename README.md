09c27ed3514b24734d1964775725a43d9893ebe6
Leo Singer <leo.singer@ligo.org>
2018-01-24 13:24:18 -0500

    Make pip installable
    Make the package pip installable, including all build and runtime
    dependencies (except lal).
    
    Also update the installation instructions in README.md.

--- README.md
+++ README.md
@@ -10,21 +10,16 @@ Requirements:
 
 Based on the DPGMM mixture model implementation of https://github.com/thaines/helit/tree/master/dpgmm 
 
-Installation
-python setup.py build_ext --inplace
+Installation (also installs dependencies if necessary):
+
+    pip install git+https://github.com/wdpozzo/3d_volume
 
 # HOT TO SETUP A VIRTUALENV ENVIRONMENT ON CLUSTERS AND INSTALLING THE REQUIRED PACKAGES THERE
 
-virtualenv destination_folder
-
-pip install numpy
-
-pip install scipy
-
-pip install healpy
-
-pip install cython
+    virtualenv destination_folder
+    source destination_folder/bin/activate
+    pip install git+https://github.com/wdpozzo/3d_volume
 
 After logging out, one will need to activate the virtual environment by typing:
 
-source destination_folder/activate
+    source destination_folder/bin/activate
--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
-from setuptools import setup, find_packages
-from distutils.extension import Extension
-from Cython.Build import cythonize
-import numpy
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 
 
-ext_modules=[
-             Extension("cumulative",
-                       sources=["cumulative.pyx"],
-                       libraries=["m"] # Unix-like specific
-                       )
-             ]
+# https://stackoverflow.com/questions/19919905
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 
 setup(
-      name = "Cumulative",
-      ext_modules = cythonize(ext_modules),
-      include_dirs=[numpy.get_include()]
+      name = "3d_volume",
+      ext_modules = [
+                     Extension("cumulative",
+                               sources=["cumulative.pyx"],
+                               libraries=["m"] # Unix-like specific
+                               )
+      ],
+      cmdclass={'build_ext': build_ext},
+      py_modules=['VolumeLocalisation',
+                  'dpgmm',
+                  'gaussian',
+                  'gaussian_inc',
+                  'gaussian_prior',
+                  'gcp',
+                  'generate_submit_file',
+                  'student_t',
+                  'wishart'
+                 ],
+      install_requires=['healpy', 'numpy', 'scipy'],
+      setup_requires=['cython', 'numpy', 'setuptools>=18.0']
       )
